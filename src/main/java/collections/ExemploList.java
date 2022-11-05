2addd19d98c8db5c4c36cbad3785c71c707a51b2
Ligia Alves <ligia.stalves@gmail.com>
2022-11-05 11:36:29 -0300

    arraylist

--- src/main/java/collections/ExemploList.java
+++ src/main/java/collections/ExemploList.java
@@ -1,6 +1,8 @@
 package collections;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 //Dada um lista comnotas de um aluno [7,8.5,9.3,5,7,0,3.6] faca:
@@ -38,5 +40,45 @@ public class ExemploList {
         //System.out.println(notas); só imprimir o array ou usar um for each
         for (Double nota : notas)
             System.out.println(nota);
+        System.out.println("Exiba a terceira nota adicionada: " + notas.get(2));
+
+        System.out.println(notas.toString());
+
+        System.out.println("Exiba a menor nota: " + Collections.min(notas));
+
+        System.out.println("Exiba a maior nota: " + Collections.max(notas));
+
+        Iterator<Double> iterator = notas.iterator();
+        Double soma = 0d;
+        while (iterator.hasNext()){ //ve se tem proximo apos o cursor
+            Double next = iterator.next();
+            soma += next;
+        }
+        System.out.println("Exiba a soma das notas: " + soma);
+
+        System.out.println("Exiba a media das notas: " + soma/notas.size());
+
+        System.out.println("Remova a nota 0: ");
+        notas.remove(0d);
+        System.out.println(notas);
+
+        System.out.println("Remova a nota na posição 0: ");
+        notas.remove(0);
+        System.out.println(notas);
+
+        System.out.println("Remova as notas menores que 7 e exiba a lista:");
+        Iterator<Double> iterator1 = notas.iterator();
+        while(iterator1.hasNext()){ //tem proximo?
+            Double next = iterator1.next(); //guarda na variavel
+            if(next < 7) iterator1.remove(); //esse proximo é menor que sete? Sim, remove
+        }
+        System.out.println(notas);
+
+//        System.out.println("Apague toda a lista");
+//        notas.clear();
+//        System.out.println(notas);
+
+        System.out.println("Confira se apagou toda a lista: " + notas.isEmpty());
+
     }
 }
