6b74cfd0087c8542f39e7baf00dceab9bf3b2447
Ligia Alves <ligia.stalves@gmail.com>
2022-12-26 21:49:54 -0300

    update exceptions

---
+++ src/main/java/excecoes/DivisaoNaoExataException.java
@@ -0,0 +1,13 @@
+package excecoes;
+
+public class DivisaoNaoExataException extends Exception{
+
+    private int numerador;
+    private int denominador;
+
+    public DivisaoNaoExataException(String message, int numerador, int denominador) {
+        super(message);
+        this.numerador = numerador;
+        this.denominador = denominador;
+    }
+}
