b04d142c29bd2c3473e46096662dc66e04cb0b63
Ligia Alves <ligia.stalves@gmail.com>
2023-01-02 14:52:37 -0300

    update

---
+++ src/test/java/testes/PessoaTest.java
@@ -0,0 +1,21 @@
+package testes;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+class PessoaTest {
+
+    @Test
+    void validarCalculoIdade(){
+        Pessoa pessoa = new Pessoa("Ligia", LocalDate.of(2020,1,1));
+        Assertions.assertEquals(3, pessoa.getIdade()); //eu espero que seja 3 anos, quando eu chamar getIdade
+    }
+
+    @Test
+    void retornarSeEhMaiorDeIdade(){
+        Pessoa pessoa = new Pessoa("Ligia", LocalDate.of(2020,1,1));
+        Assertions.assertFalse(pessoa.ehMaiorDeIdade());
+    }
+}
